6de88907847804b65dbb617178ba1fd0f22a235f
Mingjie Wang <mingjie.wang@uber.com>
2026-05-05 21:42:41 -0700

    feat(app): register ⌘⇧V hotkey to open clipboard popup

---
+++ MacAllYouNeed/Clipboard/HotkeyController.swift
@@ -0,0 +1,24 @@
+import AppKit
+import Platform
+
+@MainActor
+final class HotkeyController {
+    private let popup: ClipboardPopupController
+    private var hotkey: GlobalHotkey?
+
+    init(popup: ClipboardPopupController) {
+        self.popup = popup
+    }
+
+    func registerDefault() {
+        hotkey = GlobalHotkey(descriptor: .defaultClipboard) { [weak self] in
+            Task { @MainActor in self?.popup.show() }
+        }
+        try? hotkey?.register()
+    }
+
+    func unregister() {
+        hotkey?.unregister()
+        hotkey = nil
+    }
+}
